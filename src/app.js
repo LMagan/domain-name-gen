5d5c4e7a53161b21f0212c6c54c61a2193fff47c
L.Magán <lucasfliper@hotmail.com>
2024-11-20 09:57:03 +0000

    domainList

--- src/app.js
+++ src/app.js
@@ -6,6 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["el", "nuestro", "tu"];
+  let nombre = ["coyote", "burro", "primo"];
+  let adj = ["cojo", "viejo", "estafador"];
+  let dom = [".com", ".net", ".es"];
+
+  let total = [];
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let x = 0; x < nombre.length; x++) {
+      for (let y = 0; y < adj.length; y++) {
+        for (let h = 0; h < dom.length; h++) {
+          total.push(pronoun[i] + nombre[x] + adj[y] + dom[h]);
+        }
+      }
+    }
+  }
+  let ul = document.getElementById("d");
+  for (let z = 0; z < total.length; z++) {
+    let li = document.createElement("li");
+    li.innerHTML = total[z];
+    ul.appendChild(li);
+  }
 };
